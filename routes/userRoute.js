1c4c5a9504ccf80f5128ce56c762a5714e37f191
Harsh <harshsingh45831@gmail.com>
2026-02-24 15:46:34 +0530

    updates

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,15 @@
 import {
   addUser,
   deleteUser,
+  login,
   showUsers,
 } from "../controllers/userController.js";
+import { authenticate, authorize } from "../middleware/auth.js";
 import express from "express";
 const userRouter = express.Router();
+userRouter.get("/", authenticate, authorize("admin"), showUsers);
+userRouter.post("/login", login);
+userRouter.post("/signup", addUser);
+userRouter.delete("/:id", authenticate, authorize("admin"), deleteUser);
 
-userRouter.get("/showUsers", showUsers);
-userRouter.post("/addUser", addUser);
-userRouter.delete("/deleteUser/:id", deleteUser);
-
-export default userRouter
+export default userRouter;
